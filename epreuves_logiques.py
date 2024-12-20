142f94235379cd51449daf4097791de187ddee29
oliviabaudet <olivia.baudet@gmail.com>
2024-12-20 10:59:47 +0100

    ajout de fichiers

--- epreuves_logiques.py
+++ epreuves_logiques.py
@@ -1,3 +1,5 @@
+import random
+
 ####### Jeu des bâtonnets #######
 
 def affiche_batonnets(n):
@@ -42,6 +44,131 @@ def jeu_nim():
         print("Le maître du jeu a retiré le dernier bâtonnet. Le joueur gagne !")
         return True
 
+
+
+####### Morpion #######
+
+def affiche_grille(grille):
+    print(f" {grille[0][0]} | {grille[0][1]} | {grille[0][2]} ")
+    print("___|___|___")
+    print(f" {grille[1][0]} | {grille[1][1]} | {grille[1][2]} ")
+    print("___|___|___")
+    print(f" {grille[2][0]} | {grille[2][1]} | {grille[2][2]} ")
+    print("   |   |   ")
+
+def verifier_victoire(grille, symbole):
+    victoire = False
+    for i in range(len(grille)):
+        if grille[i][0] == symbole and grille[i][1] == symbole and grille[i][2] == symbole:
+            victoire = True
+    for i in range(len(grille)):
+        if grille[0][i] == symbole and grille[1][i] == symbole and grille[2][i] == symbole:
+            victoire = True
+    if grille[0][0] == symbole and grille[1][1] == symbole and grille[2][2] == symbole:
+        victoire = True
+    if grille[0][2] == symbole and grille[1][1] == symbole and grille[2][0] == symbole:
+        victoire = True
+    return victoire
+
+def coup_maitre(grille, symbole):
+    libre = []
+    for i in range(len(grille)):
+        for j in range(len(grille)):
+            if grille[i][j] == " ":
+                libre.append((i, j))
+
+    for position in libre:
+        test = []
+        for ligne in grille:
+            test.append(ligne[:])
+        a,b = position
+        test[a][b] = symbole
+        if verifier_victoire(test, symbole):
+            return position
+
+    for position in libre:
+        test = []
+        for ligne in grille:
+            test.append(ligne[:])
+        a,b = position
+        test[a][b] = "X"
+        if verifier_victoire(test, "X"):
+            return position
+
+    a = random.choice(libre)
+    return a
+
+def tour_joueur(grille):
+    libre = []
+    for i in range(len(grille)):
+        for j in range(len(grille)):
+            if grille[i][j] == " ":
+                libre.append((i, j))
+
+    valable = [0, 1, 2]
+    position = None
+    while position not in libre:
+        x = None
+        while x not in valable:
+            x = int(input("Saisir la ligne où vous voulez jouer (0, 1 ou 2) : "))
+        y = None
+        while y not in valable:
+            y = int(input("Saisir la colonne où vous voulez jouer (0, 1 ou 2) : "))
+        position = (x,y)
+        if position not in libre:
+            print("Position déjà occupée. Choisissez une autre case.")
+    a, b = position
+    grille[a][b] = 'X'
+    return None
+
+def tour_maitre(grille):
+    position = coup_maitre(grille, "O")
+    a, b = position
+    grille[a][b] = 'O'
+    return None
+
+def grille_complete(grille):
+    libre = []
+    for i in range(len(grille)):
+        for j in range(len(grille)):
+            if grille[i][j] == " ":
+                libre.append((i, j))
+    if libre == []:
+        return True
+    else:
+        return False
+
+def verifier_resultat(grille):
+    if verifier_victoire(grille, "O"):
+        return True
+    if verifier_victoire(grille, "X"):
+        return True
+    if grille_complete(grille):
+        return True
+    return False
+
+def jeu_tictactoe():
+    grille = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    affiche_grille(grille)
+    i = 0
+    while not verifier_resultat(grille):
+        if i%2 == 0:
+            tour_joueur(grille)
+            affiche_grille(grille)
+        else:
+            print("Tour de du maître du jeu (O)...")
+            tour_maitre(grille)
+            affiche_grille(grille)
+        i += 1
+    if verifier_victoire(grille, "X"):
+        print("Le joueur a gagné !")
+        return True
+    if verifier_victoire(grille, "O"):
+        print("Le maître a gagné !")
+        return False
+    if grille_complete(grille):
+        print("Match nul, égalité !")
+        return False
+
 jeu_nim()
-
-
+jeu_tictactoe()
