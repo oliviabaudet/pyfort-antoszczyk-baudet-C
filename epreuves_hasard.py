1dcb1a83f8df81ffdb534878412637abd00c9ca2
saraatk <contact.sara592@gmail.com>
2025-01-03 21:36:47 +0100

    j'ai changé

---
+++ epreuves_hasard.py
@@ -0,0 +1,29 @@
+import random
+
+def bonneteau():
+    bonneteaux = ['A','B', 'C']
+
+    print("Bonjour, bienvenue dans le jeu des bonneteaux.")
+    print("Vous devez deviner sous quel bonneteau (A, B ou C) se cache la clé ")
+    print("Vous avez deux essais. Bon courage !")
+    print("Bonneteaux disponibles : {}".format(', '.join(bonneteaux)))
+
+    max = 2
+    for tentative in range(1, max + 1):
+        lettre = random.choice(bonneteaux)
+        print("Tentative {}/{} :".format(tentative, max))
+        choix = input("Choisissez un bonneteau({}): ".format(','.join(bonneteaux))).strip().lower()
+            if choix in lettre:
+                if choix == lettre:
+                    print("Bravo! vous avez touvé la clé sous le bonneteau {}." .format(choix))
+                    return True
+                else:
+                print("Vous n'avez pas trouvé le bon bonneteau.")
+            else:
+                print("Votre choix n'est pas valide.")
+            tentative += 1
+    print("Vous avez perdu, la clé se trouvait sous le bonneteau {}.".format(choix))
+    return False
+
+
+
