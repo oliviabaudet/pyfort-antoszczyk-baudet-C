c141a68f0342c872429b6fe30066dbf68e4ac544
oliviabaudet <olivia.baudet@gmail.com>
2025-01-04 14:10:20 +0100

    j'ai fini les enigmes du père fouras

--- enigme_pere_fouras.py
+++ enigme_pere_fouras.py
@@ -1,11 +1,28 @@
 import json
+from random import *
 
 ####### Père Fouras #######
 
 def charger_enigmes(fichier):
     with open("data/"+fichier, 'r', encoding='utf-8') as f:
-        jeu_tv = json.load(f)
-    enigmes = []
-    for elt in jeu_tv:
-        enigmes.append([elt["question"],elt["reponse"]])
-    return enigmes
+        enigmes = json.load(f)
+    liste_enigmes = []
+    for elt in enigmes:
+        liste_enigmes.append([elt["question"],elt["reponse"]])
+    return liste_enigmes
+
+
+def enigme_pere_fouras():
+    enigmes = charger_enigmes("enigmesPF.json")
+    enigme = enigmes[randint(0, len(enigmes))]
+    essais = 3
+    print(enigme[0])
+    while essais > 0:
+        reponse = input("Quel est votre réponse : ")
+        if reponse.lower() == enigme[1].lower():
+            print("Bravo, vous avez trouvé le bon mot.")
+            return True
+        essais -= 1
+        print(f"Mauvaise réponse. Il vous reste {essais} essais")
+    print("Vous avez échoué. La réponse correcte était", enigme[1])
+    return False
